20d93ca02033b17bf57fa6d26e7a1ceec803a9e8
Niffxd <nicosanchez673@gmail.com>
2023-05-02 18:32:23 -0300

    update team photo

--- src/components/ListDevices/ListDevices.jsx
+++ src/components/ListDevices/ListDevices.jsx
@@ -4,13 +4,15 @@ import info from '../../assets/json/devices.json';
 import style from './ListDevices.module.css';
 
 export default function ListDevices () {
+  const devices = [...info.devices].reverse();
+
   return !info
     ? <Loader />
     : <div className={style.list_devices_container}>
         {
-          info.devices.toReversed().map(device => {
+          devices.map(device => {
             return (
-              <Link key={device.model} to={`${device.model}`} state={ device } preventScrollReset={false}>
+              <Link key={device.model} to={`${device.model}`} state={ device }>
                 <div className={style.device_container}>
                   <img className={style.device_img} src={`${device.images.path}/${device.model}/${device.model}_${device.images.variations[0]}.png`} alt={device.model} />
                   <h3>iPhone {device.model}</h3>
