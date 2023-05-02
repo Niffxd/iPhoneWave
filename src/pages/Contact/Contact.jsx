800d66536e2f819cc15cd341f808886861738ee7
Niffxd <nicosanchez673@gmail.com>
2023-05-02 20:11:35 -0300

    update team photo

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,9 +1,11 @@
 import style from './Contact.module.css';
+import ContactForm from '../../components/ContactForm/ContactForm.jsx'; //eslint-disable-line
 
 export default function Contact () {
   return (
     <div className={style.contact_container}>
       <h1>Contact</h1>
+      <ContactForm />
     </div>
   );
 }
