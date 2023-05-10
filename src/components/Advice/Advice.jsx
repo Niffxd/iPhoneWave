7a6d053107bd582b36b9a00104dcf79f550958d9
Niffxd <nicosanchez673@gmail.com>
2023-05-09 21:02:09 -0300

    update advices page

--- src/components/Advice/Advice.jsx
+++ src/components/Advice/Advice.jsx
@@ -1,9 +1,43 @@
-import style from './ArticleComponent.module.css';
+import style from './Advice.module.css';
 
-export default function ArticleComponent () {
+export default function Advice ({ categories }) {
   return (
-    <div className={style.article_container}>
-      <h1>Notice</h1>
+    <div className={style.advice_container}>
+      {
+        categories.map(({ icon, path, path2, text, tips }) => {
+          return tips
+            ? <div key={icon} className={style.advice}>
+                <div className={style.advice_icon}>
+                  <img src={path} alt={icon} />
+                  {path2 && <img src={path2} alt={icon}/>}
+                </div>
+                <h2>{text}</h2>
+                {
+                  tips.map(({ title, image, description, recommendations }) => {
+                    return (
+                      <div key={title} className={style.advice_content}>
+                        <h3>{title}</h3>
+                        <img src={image} alt={icon} />
+                        <p>{description}</p>
+                        <ul>
+                          {
+                            recommendations.map(recommendation => {
+                              return (
+                                <li key={recommendation}>
+                                  {recommendations.indexOf(recommendation) + 1}. {recommendation}
+                                </li>
+                              );
+                            })
+                          }
+                        </ul>
+                      </div>
+                    );
+                  })
+                }
+              </div>
+            : '';
+        })
+      }
     </div>
   );
 }
--- src/pages/Advices/Advices.jsx
+++ src/pages/Advices/Advices.jsx
@@ -1,11 +1,21 @@
+import { Link } from 'react-router-dom'; //eslint-disable-line
 import Advice from '../../components/Advice/Advice'; //eslint-disable-line
+import categories from '../../assets/json/icons.json';
 import style from './Advices.module.css';
 
 export default function Advices () {
   return (
     <div className={style.advices_container}>
-      <h1>Advices</h1>
-      <Advice />
+      <h1>Recomedaciones para cuidar tu iPhone 😄</h1>
+      <Advice categories={categories} />
+      <h2>¿Necesitas asesoramiento?</h2>
+      <Link to='https://api.whatsapp.com/message/2INV3MBYA7HVP1?autoload=1&app_absent=0' target='_blank'>
+        <button className={style.request}>Envianos un mensaje 😄</button>
+      </Link>
+      <h2>¿Te gustaría reparar tu equipo?</h2>
+      <Link to='/reparaciones'>
+        <button className={style.request}>Solicitar Presupuesto 🤓</button>
+      </Link>
     </div>
   );
 }
