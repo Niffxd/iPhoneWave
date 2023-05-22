1defe0ad183da190481e9a6e0d78709a06f323a8
Niffxd <nicosanchez673@gmail.com>
2023-05-22 00:39:19 -0300

    updating some css haha

--- src/pages/FindYourPhone/FindYourPhone.jsx
+++ src/pages/FindYourPhone/FindYourPhone.jsx
@@ -10,16 +10,20 @@ export default function FindYourPhone () {
   return (
     <div className={style.find_your_phone_container}>
       <h1>Pasos para identificar tu iPhone</h1>
-      <iframe className={style.video} src='https://www.youtube.com/embed/82Nzx0nk7U4' title='YouTube video player' frameBorder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' allowFullScreen></iframe>
-      <Link to='/reparaciones'>
-        <button>
-          Volver
-        </button>
-      </Link>
-      <h2>¿Necesitas más info?</h2>
-      <Link to='https://support.apple.com/es-es/HT201296' target='_blank'>
-        👉 Ingresa aquí 👈
-      </Link>
+      <div className={style.video_container}>
+        <iframe className={style.video} src='https://www.youtube.com/embed/82Nzx0nk7U4' title='YouTube video player' frameBorder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' allowFullScreen></iframe>
+        <div className={style.buttons}>
+          <h2>¿Necesitas más info?</h2>
+          <Link to='https://support.apple.com/es-es/HT201296' target='_blank'>
+            👉 Ingresa aquí 👈
+          </Link>
+          <Link to='/reparaciones'>
+            <button>
+              Volver
+            </button>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
